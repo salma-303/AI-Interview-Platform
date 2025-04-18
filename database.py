a84b0a8e98e1589caa8422531f2f36433065464a
shrouk hany <134003439+shroukhm@users.noreply.github.com>
2025-04-18 21:24:09 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 from supabase import create_client, Client
 
 SUPABASE_URL = "https://vtxfuhvnhfhkwapzlnlg.supabase.co"  
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ0eGZ1aHZuaGZoa3dhcHpsbmxnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM2MDczODgsImV4cCI6MjA1OTE4MzM4OH0.LBrJ7WRMjN2bfIr2UgME0tQm0AZKNUATDVSZSm_9KuE"  # Replace with your anon key
+SUPABASE_KEY = " "  # Replace with your anon key
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
